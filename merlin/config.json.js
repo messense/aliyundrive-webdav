7b89e4395fa614db20d883bb8c95cc8e3cfeaac5
messense <messense@icloud.com>
2021-08-22 19:29:41 +0800

    Build ASUS Merlin package

---
+++ merlin/config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "阿里云盘 WebDAV 服务器",
+    "home_url": "Module_aliyundrivewebdav.asp",
+    "md5": "a8925290bcaa8d5f34b26237639b6013",
+    "module": "aliyundrivewebdav",
+    "title": "阿里云盘WebDAV",
+    "version": "0.1.10"
+}
