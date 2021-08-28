4cd832e5b6d82078f6300a936ec1ca3afa956e48
messense <messense@icloud.com>
2021-08-28 10:37:31 +0800

    Allow install on koolshare merlin 384 and later

--- merlin/config.json.js
+++ merlin/config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "阿里云盘 WebDAV 服务器",
     "home_url": "Module_aliyundrivewebdav.asp",
-    "md5": "a8925290bcaa8d5f34b26237639b6013",
+    "md5": "bca108d2d988601cbb7f067d4630941b",
     "module": "aliyundrivewebdav",
     "title": "阿里云盘WebDAV",
     "version": "0.1.12"
