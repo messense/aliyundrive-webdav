f8cb60a62484e27032b458d0255e86e74ffb8bb0
messense <messense@icloud.com>
2021-09-03 21:32:59 +0800

    Move merlin arm380 to its directory

--- merlin/build.py
+++ merlin/build.py
@@ -3,9 +3,11 @@
 from __future__ import print_function
 
 import os
+import sys
 import json
 import codecs
 import hashlib
+import platform
 from string import Template
 
 parent_path = os.path.dirname(os.path.realpath(__file__))
@@ -39,24 +41,37 @@ def build_module():
     except Exception:
         print("config.json.js file format is incorrect")
         return
+
+    if len(sys.argv) != 2:
+        print("Usage: python build.py <path>")
+        return
+
+    module_root = sys.argv[1]
     if "module" not in conf:
         print("module is not in config.json.js")
         return
-    module_path = os.path.join(parent_path, conf["module"])
+    module_path = os.path.join(parent_path, module_root)
     if not os.path.isdir(module_path):
         print("%s dir not found，check config.json.js is module ?" % module_path)
         return
-    install_path = os.path.join(parent_path, conf["module"], "install.sh")
+    install_path = os.path.join(module_path, "install.sh")
     if not os.path.isfile(install_path):
         print("%s file not found，check install.sh file" % install_path)
         return
 
-    with codecs.open(os.path.join(parent_path, conf["module"], "version"), "w", "utf-8") as fw:
+    with codecs.open(os.path.join(module_path, "version"), "w", "utf-8") as fw:
         fw.write(conf["version"])
 
     print("build...")
-    t = Template("cd $parent_path && rm -f $module.tar.gz && tar -zcf $module.tar.gz $module")
-    os.system(t.substitute({"parent_path": parent_path, "module": conf["module"]}))
+    if platform.system() == "Darwin":
+        t = Template("cd $parent_path && rm -f $module.tar.gz && tar -zcf $module.tar.gz -s /^$module_root/$module/ $module_root")
+    else:
+        t = Template("cd $parent_path && rm -f $module.tar.gz && tar -zcf $module.tar.gz --transform s/$module_root/$module/ $module_root")
+    os.system(t.substitute({
+        "parent_path": parent_path,
+        "module": conf["module"],
+        "module_root": module_root,
+    }))
     conf["md5"] = md5sum(os.path.join(parent_path, conf["module"] + ".tar.gz"))
     conf_path = os.path.join(parent_path, "config.json.js")
     with codecs.open(conf_path, "w", "utf-8") as fw:
@@ -69,4 +84,3 @@ def build_module():
 
 if __name__ == '__main__':
     build_module()
-
--- merlin/config.json.js
+++ merlin/config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "阿里云盘 WebDAV 服务器",
     "home_url": "Module_aliyundrivewebdav.asp",
-    "md5": "6e0b71f5cc40658b0a6a4f98e3590643",
+    "md5": "c0bac93db83f87293a1e4efb7aa5a97c",
     "module": "aliyundrivewebdav",
     "title": "阿里云盘WebDAV",
     "version": "0.1.19"
