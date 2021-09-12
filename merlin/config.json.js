f96186943409cc3e97bce5def2aa06d68c53dafa
messense <messense@icloud.com>
2021-09-12 14:09:05 +0800

    Bump version to 0.1.25

--- merlin/config.json.js
+++ merlin/config.json.js
@@ -3,6 +3,6 @@
     "home_url": "Module_aliyundrivewebdav.asp",
     "md5": "c0bac93db83f87293a1e4efb7aa5a97c",
     "module": "aliyundrivewebdav",
-    "title": "阿里云盘WebDAV",
-    "version": "0.1.24"
+    "title": "阿里云盘 WebDAV",
+    "version": "0.1.25"
 }
