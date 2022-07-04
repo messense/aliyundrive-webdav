909e33301fe48741e00c4ac99809445898672552
messense <messense@icloud.com>
2022-07-04 20:30:24 +0800

    OpenWrt Luci 配置界面增加扫码登录获取 refresh token 功能

--- openwrt/luci-app-aliyundrive-webdav/luasrc/controller/aliyundrive-webdav.lua
+++ openwrt/luci-app-aliyundrive-webdav/luasrc/controller/aliyundrive-webdav.lua
@@ -6,15 +6,18 @@ function index()
 	end
 
 	local page
-	page = entry({"admin", "services", "aliyundrive-webdav"}, alias("admin", "services", "aliyundrive-webdav", "client"), _("AliyunDrive WebDAV"), 10) -- 首页
+	page = entry({ "admin", "services", "aliyundrive-webdav" }, alias("admin", "services", "aliyundrive-webdav", "client"),
+		_("AliyunDrive WebDAV"), 10) -- 首页
 	page.dependent = true
 	page.acl_depends = { "luci-app-aliyundrive-webdav" }
 
-	entry({"admin", "services", "aliyundrive-webdav", "client"}, cbi("aliyundrive-webdav/client"), _("Settings"), 10).leaf = true -- 客户端配置
-	entry({"admin", "services", "aliyundrive-webdav", "log"}, form("aliyundrive-webdav/log"), _("Log"), 30).leaf = true -- 日志页面
+	entry({ "admin", "services", "aliyundrive-webdav", "client" }, cbi("aliyundrive-webdav/client"), _("Settings"), 10).leaf = true -- 客户端配置
+	entry({ "admin", "services", "aliyundrive-webdav", "log" }, form("aliyundrive-webdav/log"), _("Log"), 30).leaf = true -- 日志页面
 
-	entry({"admin", "services", "aliyundrive-webdav", "status"}, call("action_status")).leaf = true -- 运行状态
-	entry({"admin", "services", "aliyundrive-webdav", "logtail"}, call("action_logtail")).leaf = true -- 日志采集
+	entry({ "admin", "services", "aliyundrive-webdav", "status" }, call("action_status")).leaf = true -- 运行状态
+	entry({ "admin", "services", "aliyundrive-webdav", "logtail" }, call("action_logtail")).leaf = true -- 日志采集
+	entry({ "admin", "services", "aliyundrive-webdav", "qrcode" }, call("action_generate_qrcode")).leaf = true -- 生成扫码登录二维码地址和参数
+	entry({ "admin", "services", "aliyundrive-webdav", "query" }, call("action_query_qrcode")).leaf = true -- 查询扫码登录结果
 end
 
 function action_status()
@@ -37,4 +40,20 @@ function action_logtail()
 	end
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
+end
+
+function action_generate_qrcode()
+	local output = luci.sys.exec("aliyundrive-webdav qr generate")
+	luci.http.prepare_content("application/json")
+	luci.http.write(output)
+end
+
+function action_query_qrcode()
+	local data = luci.http.formvalue()
+	local t = data.t
+	local ck = data.ck
+	local output = {}
+	output.refresh_token = luci.sys.exec("aliyundrive-webdav qr query --t " .. t .. " --ck " .. ck)
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(output)
 end
