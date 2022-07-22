7c1c7b1e75d743d01015ee35b10a82695896dc46
messense <messense@icloud.com>
2022-07-22 12:12:06 +0800

    Add invalidate cache button to OpenWrt luci

--- openwrt/luci-app-aliyundrive-webdav/luasrc/controller/aliyundrive-webdav.lua
+++ openwrt/luci-app-aliyundrive-webdav/luasrc/controller/aliyundrive-webdav.lua
@@ -18,6 +18,7 @@ function index()
 	entry({ "admin", "services", "aliyundrive-webdav", "logtail" }, call("action_logtail")).leaf = true -- 日志采集
 	entry({ "admin", "services", "aliyundrive-webdav", "qrcode" }, call("action_generate_qrcode")).leaf = true -- 生成扫码登录二维码地址和参数
 	entry({ "admin", "services", "aliyundrive-webdav", "query" }, call("action_query_qrcode")).leaf = true -- 查询扫码登录结果
+	entry({ "admin", "services", "aliyundrive-webdav", "invalidate-cache" }, call("action_invalidate_cache")).leaf = true -- 清除缓存
 end
 
 function action_status()
@@ -56,4 +57,11 @@ function action_query_qrcode()
 	output.refresh_token = luci.sys.exec("aliyundrive-webdav qr query --t " .. t .. " --ck " .. ck)
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(output)
+end
+
+function action_invalidate_cache()
+	local e = {}
+	e.ok = luci.sys.call("kill -HUP `pidof aliyundrive-webdav`") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
