ba1c8e7d73aac392e449efb5e214ea028c1a6008
messense <messense@icloud.com>
2023-03-07 16:23:49 +0800

    Use asyncio in `refresh_token.py`
    [ci skip]

--- backend/refresh_token.py
+++ backend/refresh_token.py
@@ -1,46 +1,70 @@
-import time
+import asyncio
 
-import requests
+import httpx
 import streamlit as st
 
 
-st.title("aliyundrive-webdav")
-st.header("refresh token 获取工具")
+session = httpx.AsyncClient()
 
-if st.button("点击生成二维码"):
-    res = requests.post(
-        "https://aliyundrive-oauth.messense.me/oauth/authorize/qrcode",
-        json={
-            "scopes": ["user:base", "file:all:read", "file:all:write"],
-            "width": 300,
-            "height": 300,
-        },
+
+async def main():
+    st.set_page_config(
+        page_title="aliyundrive-webdav refresh token 获取工具",
     )
-    res.raise_for_status()
-    data = res.json()
-    sid = data["sid"]
-    qrcode_url = data["qrCodeUrl"]
-    st.image(qrcode_url, caption="使用阿里云盘 App 扫码")
+    st.title("aliyundrive-webdav")
+    st.header("refresh token 获取工具")
 
-    while True:
-        res = requests.get(f"https://openapi.aliyundrive.com/oauth/qrcode/{sid}/status")
-        res.raise_for_status()
+    if st.button("点击获取扫码登录二维码"):
+        res = await session.post(
+            "https://aliyundrive-oauth.messense.me/oauth/authorize/qrcode",
+            json={
+                "scopes": ["user:base", "file:all:read", "file:all:write"],
+                "width": 300,
+                "height": 300,
+            },
+        )
         data = res.json()
-        status = data["status"]
-        if status == "LoginSuccess":
-            code = data["authCode"]
-            res = requests.post(
-                "https://aliyundrive-oauth.messense.me/oauth/access_token",
-                json={
-                    "grant_type": "authorization_code",
-                    "code": code,
-                },
-            )
-            res.raise_for_status()
-            data = res.json()
-            refresh_token = data["refresh_token"]
+        sid = data["sid"]
+        qrcode_url = data["qrCodeUrl"]
+        st.image(qrcode_url, caption="使用阿里云盘 App 扫码")
+
+        refresh_token = None
+        with st.spinner("等待扫码授权中..."):
+            while True:
+                res = await session.get(
+                    f"https://openapi.aliyundrive.com/oauth/qrcode/{sid}/status"
+                )
+                data = res.json()
+                status = data["status"]
+                if status == "LoginSuccess":
+                    code = data["authCode"]
+                    res = await session.post(
+                        "https://aliyundrive-oauth.messense.me/oauth/access_token",
+                        json={
+                            "grant_type": "authorization_code",
+                            "code": code,
+                        },
+                    )
+                    data = res.json()
+                    refresh_token = data["refresh_token"]
+                    break
+                elif status == "QRCodeExpired":
+                    st.error("二维码已过期, 请刷新页面后重试", icon="🚨")
+                    break
+
+                await asyncio.sleep(2)
+
+        if refresh_token:
             st.success("refresh token 获取成功", icon="✅")
             st.code(refresh_token, language=None)
-            break
 
-        time.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        import uvloop
+    except ImportError:
+        pass
+    else:
+        uvloop.install()
+
+    asyncio.run(main())
