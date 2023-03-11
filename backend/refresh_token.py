a9e6ad9f905e3c642401e6ccd2a425d48d2c4b23
messense <messense@icloud.com>
2023-03-11 12:32:50 +0800

    refresh_token: add authCode tab
    [ci skip]

--- backend/refresh_token.py
+++ backend/refresh_token.py
@@ -14,6 +14,19 @@ async def get_qrcode_status(sid: str) -> dict:
     return res.json()
 
 
+async def get_refresh_token(code: str) -> str:
+    res = await session.post(
+        "https://aliyundrive-oauth.messense.me/oauth/access_token",
+        json={
+            "grant_type": "authorization_code",
+            "code": code,
+        },
+    )
+    data = res.json()
+    refresh_token = data["refresh_token"]
+    return refresh_token
+
+
 async def main():
     st.set_page_config(
         page_title="aliyundrive-webdav refresh token 获取工具",
@@ -21,56 +34,63 @@ async def main():
     st.title("aliyundrive-webdav")
     st.header("refresh token 获取工具")
 
-    if st.button("点击获取扫码登录二维码"):
-        res = await session.post(
-            "https://aliyundrive-oauth.messense.me/oauth/authorize/qrcode",
-            json={
-                "scopes": ["user:base", "file:all:read", "file:all:write"],
-                "width": 300,
-                "height": 300,
-            },
-        )
-        data = res.json()
-        sid = data["sid"]
-        qrcode_url = data["qrCodeUrl"]
-        st.image(qrcode_url, caption="使用阿里云盘 App 扫码")
+    qrcode_tab, authcode_tab = st.tabs(["扫码授权", "authCode"])
 
-        refresh_token = None
-        with st.spinner("等待扫码授权中..."):
-            while True:
+    with qrcode_tab:
+        if st.button("点击获取扫码登录二维码"):
+            res = await session.post(
+                "https://aliyundrive-oauth.messense.me/oauth/authorize/qrcode",
+                json={
+                    "scopes": ["user:base", "file:all:read", "file:all:write"],
+                    "width": 300,
+                    "height": 300,
+                },
+            )
+            data = res.json()
+            sid = data["sid"]
+            qrcode_url = data["qrCodeUrl"]
+            st.image(qrcode_url, caption="使用阿里云盘 App 扫码")
+
+            refresh_token = None
+            with st.spinner("等待扫码授权中..."):
+                while True:
+                    try:
+                        data = await get_qrcode_status(sid)
+                    except httpx.ConnectTimeout:
+                        st.error(
+                            "查询扫码结果超时, 可能是触发了阿里云盘接口限制, 请稍后再试.\n"
+                            "或者自行尝试轮询此接口后切换到 authCode tab 获取 refresh token: "
+                            f"https://openapi.aliyundrive.com/oauth/qrcode/{sid}/status",
+                            icon="🚨",
+                        )
+                        break
+
+                    status = data["status"]
+                    if status == "LoginSuccess":
+                        code = data["authCode"]
+                        refresh_token = await get_refresh_token(code)
+                        break
+                    elif status == "QRCodeExpired":
+                        st.error("二维码已过期, 请刷新页面后重试", icon="🚨")
+                        break
+
+                    await asyncio.sleep(2)
+
+            if refresh_token:
+                st.success("refresh token 获取成功", icon="✅")
+                st.code(refresh_token, language=None)
+
+    with authcode_tab:
+        with st.form("authCode"):
+            code = st.text_input("authCode", help="填入 authCode")
+            submitted = st.form_submit_button("提交")
+            if submitted and code:
                 try:
-                    data = await get_qrcode_status(sid)
-                except httpx.ConnectTimeout:
-                    st.error(
-                        "查询扫码结果超时, 可能是触发了阿里云盘接口限制, 请稍后再试.\n"
-                        "或者自行尝试轮询此接口: "
-                        f"https://openapi.aliyundrive.com/oauth/qrcode/{sid}/status",
-                        icon="🚨",
-                    )
-                    break
-
-                status = data["status"]
-                if status == "LoginSuccess":
-                    code = data["authCode"]
-                    res = await session.post(
-                        "https://aliyundrive-oauth.messense.me/oauth/access_token",
-                        json={
-                            "grant_type": "authorization_code",
-                            "code": code,
-                        },
-                    )
-                    data = res.json()
-                    refresh_token = data["refresh_token"]
-                    break
-                elif status == "QRCodeExpired":
-                    st.error("二维码已过期, 请刷新页面后重试", icon="🚨")
-                    break
-
-                await asyncio.sleep(2)
-
-        if refresh_token:
-            st.success("refresh token 获取成功", icon="✅")
-            st.code(refresh_token, language=None)
+                    refresh_token = await get_refresh_token(code)
+                    st.success("refresh token 获取成功", icon="✅")
+                    st.code(refresh_token, language=None)
+                except KeyError:
+                    st.error("无效的 authCode, 请重新获取", icon="🚨")
 
 
 if __name__ == "__main__":
